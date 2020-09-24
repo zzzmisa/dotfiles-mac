1d4cc0f1ccad3010838925172bde352bd38b6f29
zzzmisa <zzzmisa.office@gmail.com>
2020-09-24 22:09:19 +0900

    Add install-homebrew.sh

---
+++ homebrew/install-homebrew.sh
@@ -0,0 +1,6 @@
+# Homebrewインストール
+# cf. https://brew.sh/
+if !(type brew > /dev/null 2>&1); then
+  # brewコマンドがインストールされていない場合のみ実行
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
