2c7c2ef9023a6ad8bc9e6bdcb35dcec6c5ed05b1
zzzmisa <zzzmisa.office@gmail.com>
2020-09-24 22:35:10 +0900

    Update comments

--- homebrew/install-homebrew.sh
+++ homebrew/install-homebrew.sh
@@ -1,6 +1,6 @@
+# brewコマンドがない場合のみ
 # Homebrewインストール
 # cf. https://brew.sh/
 if !(type brew > /dev/null 2>&1); then
-  # brewコマンドがインストールされていない場合のみ実行
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
--- vim/installer.sh
+++ vim/installer.sh
@@ -1,7 +1,9 @@
 # .vimrcの設置
 ln -sf $PWD/vim/.vimrc ~/.vimrc
 
-# Dein.vimインストール（プラグインインストールはVim上で :call dein#install() 実行）
+# インストールされていない場合のみ（~/.vim/deinフォルダがない場合のみ）
+# Dein.vimインストール
+# （プラグインインストールはVim上で :call dein#install() 実行）
 if [ ! -e ~/.vim/dein ]; then
   curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein.sh
   sh ./dein.sh ~/.vim/dein
