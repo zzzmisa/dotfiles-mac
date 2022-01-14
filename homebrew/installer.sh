5c513caf49bbb96d9e536d7044d3393255ebb787
zzzmisa <zzzmisa.office@gmail.com>
2022-01-15 06:21:08 +0900

    Divide brewfile for office and private

--- homebrew/installer.sh
+++ homebrew/installer.sh
@@ -5,8 +5,13 @@ source homebrew/install-homebrew.sh
 softwareupdate --install-rosetta
 
 # Brewfile実行
+read -p "Press O for office use, press any key for private use :  " install_env
 cd $PWD/homebrew
-brew bundle
+if [ "$install_env" = "O" ]; then
+  brew bundle --file BrewfileOffice
+else
+  brew bundle --file BrewfilePrivate
+fi
 cd -
 
 echo 👍 Homebrew setting is done!
