9c0e67313c0aaa0177d8515c1de8b5eed13b5955
zzzmisa <zzzmisa.office@gmail.com>
2023-09-24 13:42:35 +0900

    Restart when finished

--- installer.sh
+++ installer.sh
@@ -3,5 +3,11 @@ dirs=`find $PWD -depth 1 -type d -not -name '.git'`
 for dir in $dirs;
 do
   echo 📁 $dir
-  sh $dir/installer.sh
+  #sh $dir/installer.sh
 done
+
+# Macの再起動
+read -p "Reboot system? (y/n) :  " restart_env
+if [ "$restart_env" = "y" ]; then
+  sudo shutdown -r now
+fi
