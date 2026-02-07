bee21d7191e44d30e912b2c64284566ea66e1272
zzzmisa <zzzmisa.office@gmail.com>
2026-02-07 11:01:49 +0900

    refactor(installer): remove Dein.vim installation script

--- vim/installer.sh
+++ vim/installer.sh
@@ -3,13 +3,4 @@ set -e
 # .vimrcの設置
 ln -sf $PWD/vim/.vimrc ~/.vimrc
 
-# インストールされていない場合のみ（~/.vim/deinフォルダがない場合のみ）
-# Dein.vimインストール
-# （プラグインインストールはVim上で :call dein#install() 実行）
-if [ ! -e ~/.vim/dein ]; then
-  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein.sh
-  sh ./dein.sh ~/.vim/dein
-  rm dein.sh
-fi
-
 echo 👍 Vim setting is done!
