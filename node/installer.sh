233ddc35dd861e4a5069f9d609dbced90b4f43c5
zzzmisa <zzzmisa.office@gmail.com>
2026-02-07 14:39:47 +0900

    refactor(installer): remove nodenv and pyenv installation checks

--- node/installer.sh
+++ node/installer.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env zsh
 set -e
-# nodeコマンドがなければ
-# Homebrew経由でnodenvをインストール
-if ! type node > /dev/null 2>&1; then
-  source homebrew/install-homebrew.sh
-  brew install nodenv
-fi
 
 # Nodeのバージョンチェックとインストール/アップデート
 sh node/set-nodenv.sh
